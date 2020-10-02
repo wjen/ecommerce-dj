d00308518d7c2f677fef3328d191cb869cea6777
Wen Jen <wjen1214@gmail.com>
2020-10-02 12:38:12 -0700

    add navbar

--- auctions/views.py
+++ auctions/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Listing, Bid, User, Comment
 # Create your views here.
 def index(request):
+
     return render(request, 'index.html', {})
