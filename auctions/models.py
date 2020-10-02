a8012dbe989a89896e7e715c4329598334b922c7
Wen Jen <wjen1214@gmail.com>
2020-10-02 16:58:52 -0700

    add css and cards

--- auctions/models.py
+++ auctions/models.py
@@ -18,14 +18,14 @@ class Listing(models.Model):
         ('TRAVEL', 'Travel'),
         ('CLOTHING', 'Clothes'),
     ]
-    title: models.CharField(max_length=200)
-    price: models.DecimalField(decimal_places=2, max_digits=10)
-    image: models.URLField(blank=True, verbose_name='Image URL', null=True)
-    creator: models.ForeignKey(User, on_delete=models.CASCADE, related_name='listings')
-    active: models.BooleanField(default=True)
-    timestamp: models.DateTimeField(auto_now_add=True)
-    description: models.TextField()
-    category: models.CharField(choices=LISTING_CATEGORIES, blank=True)
+    title = models.CharField(max_length=200)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    image = models.URLField(blank=True, verbose_name='Image URL', null=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='listings', null=True)
+    active = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
+    category = models.CharField(max_length=16, choices=LISTING_CATEGORIES, blank=True)
     winner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True, null=True)
 
@@ -33,19 +33,19 @@ class Listing(models.Model):
         return self.title
 
 class Bid(models.Model):
-    bid_price: models.DecimalField(decimal_places=2, max_digits=10)
-    listing: models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='bids')
-    timestamp: models.DateTimeField(auto_now_add=True)
+    bid_price = models.DecimalField(decimal_places=2, max_digits=10)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='bids')
+    timestamp = models.DateTimeField(auto_now_add=True)
     bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name='placed_bids')
 
     def __str__(self):
         return f'{self.bidder} placed bid of ${self.bid_price} for {self.listing}'
 
 class Comment(models.Model):
-    comment: models.CharField(max_length=255, blank=True)
-    commenter: models.ForeignKey(User, on_delete=models.CASCADE)
-    listing: models.ForeignKey(Listing, on_delete=models.CASCADE)
-    timestamp: models.DateTimeField(auto_now_add=True)
+    comment = models.CharField(max_length=255, blank=True)
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.commenter} - {self.content} ({self.timestamp.date()})'
+        return f'{self.commenter} - {self.comment} ({self.timestamp.date()})'
--- auctions/views.py
+++ auctions/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render, redirect
 from .models import Listing, Bid, User, Comment
 # Create your views here.
 def index(request):
+    listing_list = Listing.objects.filter(active=True)
+    return render(request, 'index.html', {'listing_list': listing_list})
 
-    return render(request, 'index.html', {})
